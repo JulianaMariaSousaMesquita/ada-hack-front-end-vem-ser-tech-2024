b4d93a38b1dbd8db4951b7f00a400ddbc6608cf8
WildBarreto <wildsbsrreto@gmail.com>
2024-04-12 18:01:54 -0300

    Side bar

--- src/routes/routes.index.tsx
+++ src/routes/routes.index.tsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import Home from "../pages/home";
+import Home from "../pages/Home/home";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
   },
+  
   
 ]);
 
