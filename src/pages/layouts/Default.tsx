db8e3b7695fd756d5b97b30fb4dde8635f23b7d5
Paulo Lemes <paulo-lemes@live.com>
2024-04-13 13:53:54 -0300

    deleted header component and updated defaultlayout

--- src/pages/layouts/Default.tsx
+++ src/pages/layouts/Default.tsx
@@ -1,11 +1,11 @@
-import Header from "../../components/Header";
+import MenuBar from "../../components/MenuBar";
 import Footer from "../../components/Footer";
 import { Outlet } from "react-router-dom";
 
 export default function DefaultLayout() {
   return (
     <>
-      <Header />
+      <MenuBar />
       <Outlet />
       <Footer />
     </>
