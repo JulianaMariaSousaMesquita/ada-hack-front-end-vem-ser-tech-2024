8b8b5ee714b236404fd4c65888e4729483871b66
Juliana Maria de Sousa Mesquita <mesquita.juliana1995@gmail.com>
2024-04-13 18:40:43 -0300

    fix: adjust form module

--- src/components/Form/Form.modules.tsx
+++ src/components/Form/Form.modules.tsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import './Form.modules.css';
-import RatingStars from '../RatingStars/RatingStars.modules';
+import React, { useState } from "react";
+import axios from "axios";
+import "./Form.modules.css";
+import RatingStars from "../RatingStars/RatingStars.modules";
 
 const Form = () => {
   const [message, setMessage] = useState("");
@@ -34,33 +34,30 @@ const Form = () => {
   };
 
   return (
-    <>
-      <MenuBar
-      <div className="form-container">
-        <div className="sidebar"></div>
-        <form onSubmit={handleSubmit} className="main-content">
-          <div className="feedback-container">
-            <span className="back-arrow">&#8592;</span>
-            <h1>Feedback</h1>
-          </div>
-          <div>
-            <h4>How was your experience</h4>
-            <RatingStars onRate={handleRate} />
-          </div>
-          <textarea
-            name="message"
-            value={message}
-            onChange={handleChange}
-            className="text-area"
-            rows={6}
-            placeholder="Suggest anything we can improve..."
-          />
-          <button className="button-submit" type="submit">
-            Send Feedback
-          </button>
-        </form>
-      </div>
-    </>
+    <div className="form-container">
+      <div className="sidebar"></div>
+      <form onSubmit={handleSubmit} className="main-content">
+        <div className="feedback-container">
+          <span className="back-arrow">&#8592;</span>
+          <h1>Feedback</h1>
+        </div>
+        <div>
+          <h4>How was your experience</h4>
+          <RatingStars onRate={handleRate} />
+        </div>
+        <textarea
+          name="message"
+          value={message}
+          onChange={handleChange}
+          className="text-area"
+          rows={6}
+          placeholder="Suggest anything we can improve..."
+        />
+        <button className="button-submit" type="submit">
+          Send Feedback
+        </button>
+      </form>
+    </div>
   );
 };
 
