e5261ccd6aa1ef0ae77110f095a1a7574c2c9904
Lucas Justino <lucas.justino.dev@gmail.com>
2024-04-15 12:51:43 -0300

    Created New Feedback

--- src/components/FormFeedback/index.tsx
+++ src/components/FormFeedback/index.tsx
@@ -1,7 +1,18 @@
-import CardFeedback from "../CardFeedback";
-
+import React, { useState } from 'react';
+import CardFeedback from '../CardFeedback';
+import NewFeedback from '../NewFeedback';
 
 const FormFeedback: React.FC = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleModalOpen = () => {
+        setIsModalOpen(true);
+    };
+
+    const handleModalClose = () => {
+        setIsModalOpen(false);
+    };
+
     return (
         <div>
             <div className="bg-custom-blue overflow-hidden flex justify-center items-center mt-1">
@@ -15,12 +26,13 @@ const FormFeedback: React.FC = () => {
                 </div>
             </div>
             <div className="bg-custom-blue overflow-hidden flex justify-center items-center h-40">
-                <button className="btn bg-custom-purple rounded-full h-5 w-96">
+                <button className="btn bg-custom-purple rounded-full h-5 w-96" onClick={handleModalOpen}>
                     <span className="text-white">
-                        Ver mais Feedbacks
+                        Adicionar Feedback
                     </span>
                 </button>
             </div>
+            <NewFeedback isOpen={isModalOpen} onClose={handleModalClose} />
         </div>
     );
 };
