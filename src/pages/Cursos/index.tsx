0e5e69b85d798f289f5b4717c216ea88db2902d7
Juliana Maria de Sousa Mesquita <mesquita.juliana1995@gmail.com>
2024-04-17 13:09:32 -0300

    fix: adjust import db cursos

--- src/pages/Cursos/index.tsx
+++ src/pages/Cursos/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import CardCursos from "../../components/CardCursos";
-import { cursosData } from "./dbCursos.tsx";
+import { cursosData } from "./dbCursos";
 import { IoSearch } from "react-icons/io5";
 
 interface Curso {
